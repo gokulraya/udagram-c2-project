2bc8e25ac1fc23000ecfe6f7cc6dc3ad1f02ef99
gkumarkrishnan <gkumar@qubole.com>
2020-04-01 17:46:58 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# Udagram Image Filtering Microservice
+
+### Setup Node Environment
+
+You'll need to create a new node server. Open a new terminal within the project directory and run:
+
+1. Initialize a new project: `npm i`
+2. run the development server with `npm run dev`
+
+### Test the endpoint
+Once the server is running, open a browser and navigate to http://localhost:8082/filteredimage?image_url=your_image_url
+
+### AWS Beanstalk endpoint
+http://image-filter-udacity-gokul.us-west-2.elasticbeanstalk.com/filteredimage?image_url=
+
+
---
+++ src/server.ts
@@ -0,0 +1,75 @@
+import express from 'express';
+import bodyParser from 'body-parser';
+import {isUri} from 'valid-url';
+import {filterImageFromURL, deleteLocalFiles} from './util/util';
+
+(async () => {
+
+  // Init the Express application
+  const app = express();
+
+  // Set the network port
+  const port = process.env.PORT || 8082;
+  
+  // Use the body parser middleware for post requests
+  app.use(bodyParser.json());
+
+  // @TODO1 IMPLEMENT A RESTFUL ENDPOINT
+  // GET /filteredimage?image_url={{URL}}
+  // endpoint to filter an image from a public url.
+  // IT SHOULD
+  //    1
+  //    1. validate the image_url query
+  //    2. call filterImageFromURL(image_url) to filter the image
+  //    3. send the resulting file in the response
+  //    4. deletes any files on the server on finish of the response
+  // QUERY PARAMATERS
+  //    image_url: URL of a publicly accessible image
+  // RETURNS
+  //   the filtered image file [!!TIP res.sendFile(filteredpath); might be useful]
+
+  /**************************************************************************** */
+
+
+  app.get( "/filteredimage", async ( req, res ) => {
+    //Fetch the image_url in a var
+    const image_url = req.query.image_url;
+    console.log(image_url);
+    //Check if the URL is valid
+    if(!isUri(image_url) || !image_url){
+        return res.status(400).send("URL not found or malformed")
+    }
+    try {
+    const filteredImage = await filterImageFromURL(image_url);
+    res.sendFile(filteredImage, async (error) => {
+      await deleteLocalFiles([filteredImage]);
+      if (!error) {
+        res.status(200);
+      }
+      res.status(500).send("Something went wrong");
+    });
+  }
+  catch (error){
+    res.status(422).send("Unable to process image");
+  }});
+
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  });
+
+    //! END @TODO1
+
+  
+  // Root Endpoint
+  // Displays a simple message to the user
+  app.get( "/", async ( req, res ) => {
+    res.send("try GET /filteredimage?image_url={{}}")
+  });
+  
+
+  // Start the Server
+  app.listen( port, () => {
+      console.log( `server running http://localhost:${ port }` );
+      console.log( `press CTRL+C to stop server` );
+  } );
+})();
